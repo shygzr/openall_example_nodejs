73add52cfdddd1de96ce3b9abad07e7879a9a4f1
MyPC <shygzr@gmail.com>
2018-11-27 17:17:35 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+            济南大学智慧校园开放平台-----NodeJS应用Demo-----React框架
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
